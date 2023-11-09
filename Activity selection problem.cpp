be88b1e4856ed2679570db47bb5fae98f1780f23
Mahfuzar Rahman <113121842+Mahfuzar148@users.noreply.github.com>
2023-11-10 00:00:17 +0600

    Update Activity selection problem.cpp

--- Activity selection problem.cpp
+++ Activity selection problem.cpp
@@ -13,18 +13,20 @@ int main() {
     }
 
     sort(p.begin(), p.end());
-    ans.push_back({p[0].first, p[0].second});
+    ans.push_back(p[0]);
+    int l = p[0].first, f=p[0].second;
 
      for(int i=1;i<n;i++)
      {
-          if (p[i-1].first <= p[i].second) {
-            ans.push_back({p[i].first, p[i].second});
+          if (l <= p[i].second) {
+                l=p[i].first,f=p[i].second;
+            ans.push_back( p[i]);
 
         }
 
      }
     if (ans[ans.size()-1].first<=p[n-1].second) {
-        ans.push_back({p[n-1].first, p[n-1].second});
+        ans.push_back(p[n-1]);
     }
 
     for (auto it : ans) {
