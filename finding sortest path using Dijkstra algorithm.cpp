481bd94e531fffc3a186a005e2cb53f3a4a8fb65
Mahfuzar Rahman <113121842+Mahfuzar148@users.noreply.github.com>
2023-11-16 01:24:21 +0600

    Update finding sortest path using Dijkstra algorithm.cpp

--- finding sortest path using Dijkstra algorithm.cpp
+++ finding sortest path using Dijkstra algorithm.cpp
@@ -1,78 +1,81 @@
-#include <iostream>
-#include <vector>
-#include <queue>
-#include <climits>
-
+#include<bits/stdc++.h>
 using namespace std;
+#define endl '\n'
+#define ll long long
+const int inf = 2e9;
+const ll infLL = 9e18;
 
-class Solution {
-public:
-    vector<int> dijkstra(int V, vector<vector<int>> adj[], int S, int dest) {
-        vector<int> dist(V, INT_MAX);
-        vector<int> parent(V, -1);  // To store the parent of each vertex
-        dist[S] = 0;
+const int mx = 1e5 + 123;
+ll dist[mx];
+vector<pair<int, int>> adj[mx];
+int par[mx];
 
-        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
-        pq.push({0, S});
+void dijkstra(int s, int n)
+{
+    for (int i = 1; i <= n; i++) dist[i] = infLL;
 
-        while (!pq.empty()) {
-            int u = pq.top().second;
-            int curDis = pq.top().first;
-            pq.pop();
+    dist[s] = 0;
+    priority_queue<pair<ll, int>, vector<pair<ll, int>>, greater<pair<ll, int>>> pq;
+    pq.push({0, s});
 
-            if (curDis > dist[u]) continue;
+    while (!pq.empty())
+    {
+        int u = pq.top().second;
+        ll curDis = pq.top().first; // Corrected data type here
+        pq.pop();
 
-            for (auto &edge : adj[u]) {
-                int v = edge[0];
-                int weight = edge[1];
+        if (dist[u] < curDis) // Corrected comparison here
+            continue;
 
-                if (dist[u] != INT_MAX && curDis + weight < dist[v]) {
-                    dist[v] = curDis + weight;
-                    parent[v] = u;  // Update the parent of vertex v
-                    pq.push({dist[v], v});
-                }
+        for (auto v : adj[u])
+        {
+            if (curDis + v.second < dist[v.first])
+            {
+                dist[v.first] = curDis + v.second;
+                par[v.first] = u;
+                pq.push({dist[v.first], v.first});
             }
         }
-
-        // Trace the path from destination to source
-        vector<int> path;
-        int current = dest;
-        while (current != -1) {
-            path.push_back(current);
-            current = parent[current];
-        }
-        reverse(path.begin(), path.end());  // Reverse to get the path from source to destination
-
-        return path;
     }
-};
+}
 
-int main() {
-    int t;
-    cin >> t;
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
 
-    while (t--) {
-        int V, E;
-        cin >> V >> E;
-        vector<vector<int>> adj[V];
+    int n, m;
+    cin >> n >> m;
 
-        for (int i = 0; i < E; i++) {
-            int u, v, w;
-            cin >> u >> v >> w;
-            adj[u].push_back({v, w});
-            adj[v].push_back({u, w}); // Assuming the graph is undirected
+    for (int i = 1; i <= m; i++)
+    {
+        int u, v, w;
+        cin >> u >> v >> w;
+        adj[u].push_back({v, w});
+        adj[v].push_back({u, w});
+    }
+
+    dijkstra(1, n);
+
+    if (dist[n] == infLL)
+    {
+        cout << "-1" << endl;
+    }
+    else
+    {
+        int u = n;
+        vector<int> ans = {n};
+        while (par[u] != 0)
+        {
+            ans.push_back(par[u]);
+            u = par[u];
         }
+        reverse(ans.begin(), ans.end());
 
-        int source, destination;
-        cin >> source >> destination;
-
-        Solution obj;
-        vector<int> path = obj.dijkstra(V, adj, source, destination);
-
-        // Output the shortest path
-        cout << "Shortest path from " << source << " to " << destination << ": ";
-        for (int node : path) {
-            cout << node << " ";
+        for (auto u : ans)
+        {
+            cout << u << " ";
         }
         cout << endl;
     }
